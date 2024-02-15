264b4dff38a1a74267fef622907067f2826a62a1
antoni37 <141584861+antoni37@users.noreply.github.com>
2024-02-15 19:30:53 +0100

    u

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchBreeds = () => {
   axios.defaults.headers.common['x-api-key'] =
-    'live_3WqxKBPPm7Nhb7bH6v5KJekaENAwu83GPEJU9IDPWyNtMxR4uV3pJr6uxDYogigc';
+    'live_hh9CJ1Zv3VjgdteBeK5GM69kWDvndHkSFUjeUTCMstCGHpdv6vj31di8mmLPVDpC';
 
   return axios.get(`https://api.thecatapi.com/v1/breeds`).then(res => res.data);
 };
